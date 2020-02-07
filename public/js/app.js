2c57dba8921c9ae17312b92d336bceb43df0ad49
Eden Ramoneda <edenlee143@gmail.com>
2020-02-06 22:14:20 -0800

    added app 1.0

--- public/js/app.js
+++ public/js/app.js
@@ -59643,14 +59643,15 @@ __webpack_require__.r(__webpack_exports__);
 /*!******************************************************!*\
   !*** ./resources/js/components/myinfo/OtherInfo.vue ***!
   \******************************************************/
-/*! exports provided: default */
+/*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _OtherInfo_vue_vue_type_template_id_a9684bd8___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./OtherInfo.vue?vue&type=template&id=a9684bd8& */ "./resources/js/components/myinfo/OtherInfo.vue?vue&type=template&id=a9684bd8&");
 /* harmony import */ var _OtherInfo_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./OtherInfo.vue?vue&type=script&lang=js& */ "./resources/js/components/myinfo/OtherInfo.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _OtherInfo_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _OtherInfo_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -59680,7 +59681,7 @@ component.options.__file = "resources/js/components/myinfo/OtherInfo.vue"
 /*!*******************************************************************************!*\
   !*** ./resources/js/components/myinfo/OtherInfo.vue?vue&type=script&lang=js& ***!
   \*******************************************************************************/
-/*! exports provided: default */
+/*! no static exports found */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
